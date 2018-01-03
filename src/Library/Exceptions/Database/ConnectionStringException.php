93d0872ac803c85007a3d9b81dd9159f71a3e05c
Geraldo Andrade <geraldo@geraldoandrade.com>
2018-01-03 00:39:41 -0200

    Basic structure for database connections

---
+++ src/Library/Exceptions/Database/ConnectionStringException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Contabilissimo\Library\Exceptions;
+
+
+use RuntimeException;
+
+/**
+ * ConnectionStringException
+ *
+ * Exception used when connection string is wrong
+ */
+class ConnectionStringException extends RuntimeException
+{
+}
